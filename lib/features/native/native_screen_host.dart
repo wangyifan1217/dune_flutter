da34b09850d474c9e27240184382f939faa3a2dd
wangyifan <wangyifan@heunion.com>
2026-07-10 17:13:37 +0800

    fix

--- lib/features/native/native_screen_host.dart
+++ lib/features/native/native_screen_host.dart
@@ -1844,6 +1844,7 @@ class _NativeB2PageState extends State<_NativeB2Page> {
       offset: const Offset(0, 4),
       shape: const _WechatMenuShape(),
       menuPadding: const EdgeInsets.only(top: 12, bottom: 8),
+      color: const Color(0xFF303030),
       onSelected: (action) {
         switch (action) {
           case _B2MenuAction.scanWorkstation:
@@ -2394,7 +2395,7 @@ class _B2MenuEntry extends StatelessWidget {
   const _B2MenuEntry({
     required this.icon,
     required this.label,
-    this.color = DunesColors.text,
+    this.color = const Color(0xFFF5F5F5),
   });
 
   final IconData icon;
